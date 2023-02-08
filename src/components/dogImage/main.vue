8a1fdc5c263eb3beafb4f731cff9ce96b97087a1
david660 <daodu.muyiwa@gmail.com>
2023-02-09 00:29:34 +0100

    Created an image component in preparation for lazy-loading implementation

---
+++ src/components/dogImage/main.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const store = useStore()
+
+defineProps<{src:string}>()
+
+const showBreed = (url:string) =>{
+  let breed =url.split("/")[4]
+  router.push(`/breed/${breed}`)
+}
+
+</script>
+
+<template>
+    <div v-for="dog in store.state.dogs" class="image_cont">
+        <img @click="showBreed(dog)" :src="dog" class="img" lazyload/>
+    </div>
+</template>
+
+
+<style scoped>
+    .image_cont {
+  width: 100%;
+  border-radius: 12px;
+  background-color: white;
+  transition: 0.2s ease;
+}
+
+@media (min-width: 640px) {
+    .image_cont {
+        width:100%;
+    }
+}
+
+@media (min-width: 1024px) {
+    .image_cont {
+        width:100%;
+    }
+}
+@media (min-width: 1280px) {
+    .image_cont {
+        width:100%;
+    }
+}
+@media (min-width: 1536px) {
+    .image_cont {
+        width:100%;
+    }
+}
+
+.image_cont:hover{
+  transform: scale(1.04);
+  filter:drop-shadow(0px 4px 4px #cecece);
+}
+
+
+.img{
+  display: block;
+  width: 100%;
+  height:300px;
+  border-radius: 12px;
+  object-fit: cover;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { onMounted } from "vue";
 import { useStore } from "vuex";
-import { useRouter } from "vue-router";
+import DogImage from '../components/dogImage/main.vue'
 
 const store = useStore()
-const router = useRouter()
-
-const loading = ref(false)
 
 onMounted(()=>{
   store.dispatch("fetchDogs")
   store.dispatch("fetchDogs")
 })
 
-const showBreed = (url:string) =>{
-  let breed =url.split("/")[4]
-  router.push(`/breed/${breed}`)
-  
-}
-
 </script>
 
 <template>
-
     <div class="images_cont">
-      <div v-for="dog in store.state.dogs" class="image_cont">
-        <img @click="showBreed(dog)" :src="dog" class="img" lazyload/>
-      </div>
+      <DogImage v-for="dog in store.state.dogs" :key="dog" :src="dog"/>
     </div>
-
 </template>
 
 <style scoped>
@@ -46,11 +32,6 @@ const showBreed = (url:string) =>{
         grid-template-columns: 1fr 1fr;
     }
 }
-/* @media (min-width: 768px) {
-    .images_cont {
-        max-width: 768px;
-    }
-} */
 @media (min-width: 1024px) {
     .images_cont {
         grid-template-columns: 1fr 1fr 1fr;
@@ -67,46 +48,5 @@ const showBreed = (url:string) =>{
     }
 }
 
-.image_cont {
-  width: 100%;
-  border-radius: 12px;
-  background-color: white;
-  transition: 0.2s ease;
-}
 
-@media (min-width: 640px) {
-    .image_cont {
-        width:100%;
-    }
-}
-
-@media (min-width: 1024px) {
-    .image_cont {
-        width:100%;
-    }
-}
-@media (min-width: 1280px) {
-    .image_cont {
-        width:100%;
-    }
-}
-@media (min-width: 1536px) {
-    .image_cont {
-        width:100%;
-    }
-}
-
-.image_cont:hover{
-  transform: scale(1.04);
-  filter:drop-shadow(0px 4px 4px #cecece);
-}
-
-
-.img{
-  display: block;
-  width: 100%;
-  height:300px;
-  border-radius: 12px;
-  object-fit: cover;
-}
 </style>
